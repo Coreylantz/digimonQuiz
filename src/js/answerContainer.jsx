27277815c5762338850e68f266dafe28890be299
Daniel Elliott <daniel.elliott.work@gmail.com>
2016-10-23 14:59:33 -0400

    className fix

--- src/js/answerContainer.jsx
+++ src/js/answerContainer.jsx
@@ -4,7 +4,7 @@ import {render} from 'react-dom';
 export default class AnswerContainer extends React.Component {
   render(){
     return(
-      <div className="AnswerContainer">Answers go here</div>
+      <div className="answerContainer">Answers go here</div>
     );
   }
 } 
--- src/js/digimonContainer.jsx
+++ src/js/digimonContainer.jsx
@@ -4,7 +4,7 @@ import {render} from 'react-dom';
 export default class DigimonContainer extends React.Component {
   render(){
     return(
-      <div className="DigimonContainer">Digimon go here</div>
+      <div className="digimonContainer">Digimon go here</div>
     );
   }
 } 
