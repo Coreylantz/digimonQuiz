182b5ca3fccda8737537bb8a704eedc6b145ee6c
Corey Lantz <corey.lantz12@gmail.com>
2016-11-15 17:28:14 -0500

    adding numbers and such

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -6,23 +6,36 @@ import AnswerContainer from './answer/answer-container.jsx';
 import DigimonContainer from './digimon/digimon-container.jsx';
 
 export default class App extends React.Component {
-  getCurrentQuestion() {
+  getCurrentQuestion(n) {
     console.log(questionData);
+
     let currentQuestion = questionData[0].question;
     return currentQuestion;
   }
 
-  getAnswers() {
-    return new Seq([
-      'Test 0',
-      'Test 1',
-      'Test 2',
-      'Test 3',
-      'Test 4',
-      'Test 5',
-      'Test 6',
-      'Test 7'
-    ]);
+  getAnswers(n) {
+    let answersArray = questionData[n].answers;
+    let answerSeq = [];
+    for (var i = 0; i < answersArray.length; i++) {
+      let currentAnswer = answersArray[i];
+      answerSeq.push(currentAnswer.answerString);
+    }
+    console.log(answerSeq);
+    answerSeq = new Seq(answerSeq);
+    console.log(answerSeq);
+    return answerSeq;
+    // console.log(currentAnswer[0].answerString);
+
+    // return new Seq([
+    //   'Test 0',
+    //   'Test 1',
+    //   'Test 2',
+    //   'Test 3',
+    //   'Test 4',
+    //   'Test 5',
+    //   'Test 6',
+    //   'Test 7'
+    // ]);
   }
 
   render() {
@@ -31,8 +44,8 @@ export default class App extends React.Component {
         <DigimonContainer />
         <QuestionContainer question={this.getCurrentQuestion()} />
         <AnswerContainer
-          question={this.getCurrentQuestion()}
-          answers={this.getAnswers()}
+          question={this.getCurrentQuestion(0)}
+          answers={this.getAnswers(0)}
           />
       </div>);
   }
