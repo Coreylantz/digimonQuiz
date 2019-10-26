26965d4cc2b970a6c0f250057ba6fae105f320bd
Corey Lantz <clantz@homex.com>
2019-10-26 16:01:02 -0400

    Moving components

--- src/components/app.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components/macro'
-
-export const heading = styled.h1`
-  color: red;
-`
---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { DigimonQuiz } from '../pages/DigimonQuiz'
+
+function App() {
+  return (
+    <div className="App">
+      <DigimonQuiz />
+    </div>
+  );
+}
+
+export default App;
